9990d2462392fbe7d5590f8b5a84c1526e724aef
David Lima <102266658+Davidbenone@users.noreply.github.com>
2022-03-23 22:11:38 -0300

    Update ProjetoProgramacao.c

--- ProjetoProgramacao.c
+++ ProjetoProgramacao.c
@@ -7,28 +7,28 @@
 
 
 int main(){
-	int comando; 
-	int repetir; 
-	int n; 
-	int n2; 
-	int n3;
-	int numeroAleatorio; 
- 	int resposta; 
- 	int chances; 
- 	char nome1[30]; 
-	char nome2[30];
-	int jogadorComecar; 
-	int j1dado1, j1dado2; 
-	int j2dado1, j2dado2; 
-	int jogarDados; 
-	int soma1;
-	int soma2; 
-	int somaturno1;
-	int somaturno2; 
-	int resposta1; 
-    int resposta2; 
-	int continuarFinalizar; 
-	int senha; 
+	int comando; //Usada para escolher qual jogo voce quer jogar
+	int repetir; //Usada para jogar novamente ou voltar para o menu
+	int n; //Usada para recceber a resposta da primeira pergunta do jogador no jogo 1
+	int n2; //Usada para recceber a resposta da segunda pergunta do jogador no jogo 1
+	int n3; //Usada para recceber a resposta da terceira pergunta do jogador no jogo 1
+	int numeroAleatorio; //Usada no gerador de numeros aleatorios para o jogo 2
+ 	int resposta; //Usada para recceber as respostas do jogador no jogo 2
+ 	int chances; //define a quantidade de chances que o jogador tem no jogo 2
+ 	char nome1[30]; //Nome do jogador 1 para o jogo 3
+	char nome2[30]; //Nome do jogador 2 para o jogo 3
+	int jogadorComecar; //Usada para escolher qual jogador vai comecar no jogo 3
+	int j1dado1, j1dado2; //Dados 1 e 2 para o jogador 1
+	int j2dado1, j2dado2; //Dados 1 e 2 para o jogador 1
+	int jogarDados; //Jogar os dados no jogo 3
+	int soma1; //Soma total dos pontos no jogo 3 para o jogador 1
+	int soma2; //Soma total dos pontos no jogo 3 para o jogador 2
+	int somaturno1; //Soma dos pontos em cada turno no jogo 3 para o jogador 1
+	int somaturno2; //Soma dos pontos em cada turno no jogo 3 para o jogador 1
+	int resposta1; //Usada para recceber a resposta da primeira pergunta do jogador no easter egg
+    int resposta2; //Usada para recceber a resposta da segunda pergunta do jogador no easter egg
+	int continuarFinalizar; //Usada para continuar ou finalizar o turno no jogo 3
+	int senha; //Senha do easter egg
 	
 	menu:
 	system("cls");
@@ -43,7 +43,7 @@ int main(){
 		scanf("%d", &repetir);
 		if(repetir >= 0)
 		{
-			goto menu; 
+			goto menu; //volta para o menu
 		}
 		
 	}
@@ -51,7 +51,7 @@ int main(){
 	sair:
 	if(comando == 4)
 	{
-		exit(0); 
+		exit(0); //fecha a aplicacao
 	}
 	
 	jogo1:
@@ -100,13 +100,13 @@ int main(){
 		if(repetir == 1)
 		{
 			system("cls");
-			goto jogo1; 
+			goto jogo1; //Jogar novamente
 		}
 	
 		else
 		{
 			system("cls");
-			goto menu; 
+			goto menu; //Voltar para o menu
 	
 		}
 	}
@@ -114,11 +114,11 @@ int main(){
 	jogo2:
    	if (comando == 2)
    	{
-   		srand(time(NULL)); 
+   		srand(time(NULL)); //Gerador de numero aleatorio
 
- 		numeroAleatorio = rand() % 1001; 
+ 		numeroAleatorio = rand() % 1001; //Limita ate 1000
 	
-		for(chances = 1;chances <= 5; chances++) 
+		for(chances = 1;chances <= 5; chances++) //Limita o numero de chances em 5
 		{
 			printf("\n\nDigite sua resposta: ");
  			scanf("%d", &resposta);
@@ -150,14 +150,14 @@ int main(){
 		scanf("%d", &repetir);
 		if(repetir == 1)
 		{
-			system("cls"); 
-			goto jogo2; 
+			system("cls"); //Limpar a tela
+			goto jogo2; //Jogar novamente
 		}
 	
 		else
 		{
-			system("cls");
-			goto menu; 
+			system("cls"); //Limpar a tela
+			goto menu; //Voltar para o menu
 		
 		}
 	}
@@ -166,8 +166,8 @@ int main(){
 	jogo3:
 	if (comando == 3)
 	{
-		system("cls"); 
-		fflush(stdin); 
+		system("cls"); //Limpar a tela
+		fflush(stdin); //Limpa os inputs anteriores
 		
 		soma1 = 0;
 		soma2 = 0;
@@ -180,9 +180,9 @@ int main(){
 		printf("Digite o nome do jogador 2: ");
 		gets(nome2);
 	
-		srand(time(NULL)); 
+		srand(time(NULL)); //Gerador de numero aleatorio
 	
-	 	jogadorComecar = rand() % 2; 
+	 	jogadorComecar = rand() % 2; //Vai gerar o numero 0 ou 1
  	
 	 	printf("%d\n", jogadorComecar);
  	
@@ -202,16 +202,16 @@ int main(){
 	
 		if(jogarDados>=0)
 		{
-			j1dado1 = rand() % 6; 
-		 	j1dado2 = rand() % 6;
+			j1dado1 = rand() % 6; //Gera um numero entre 0 e 5 aleatoriamente
+		 	j1dado2 = rand() % 6; //Gera um numero entre 0 e 5 aleatoriamente
 		}
 	
-		j1dado1++; 
-		j1dado2++; 
+		j1dado1++; //Soma 1 ao numero gerado
+		j1dado2++; //Soma 1 ao numero gerado
 		
 		printf("\nDado 1: %d \t Dado 2: %d", j1dado1, j1dado2);
 		
-		if(j1dado1 == 1 && j1dado2 == 1) 
+		if(j1dado1 == 1 && j1dado2 == 1) //Se cair 1 nos dois dados o jogador perde todos os pontos
 		{
 			j1dado1 = 0;
 			j1dado2 = 0;
@@ -226,13 +226,13 @@ int main(){
 			scanf("%d", &repetir);
 			if(repetir >= 0)
 			{
-				system("cls"); 
-				goto jogo32; 
+				system("cls"); //Limpa a tela
+				goto jogo32; //Comeca o turno do jogador 2
 			}
 			
 		}
 		
-		else if(j1dado1 == 1 || j1dado2 == 1)
+		else if(j1dado1 == 1 || j1dado2 == 1) //Se cair 1 em um dos dados o jogador perde os pontos do turno
 		{
 			j1dado1 = 0;
 			j1dado2 = 0;
@@ -246,8 +246,8 @@ int main(){
 			scanf("%d", &repetir);
 			if(repetir >= 0)
 			{
-				system("cls");
-				goto jogo32; 
+				system("cls"); //Limpa a tela
+				goto jogo32; //Comeca o turno do jogador 2
 			}
 		}
 		else
@@ -257,7 +257,7 @@ int main(){
 			printf("\n\nSoma dos dados: %d\nSoma do turno: %d", soma1, somaturno1);
 		}
 		
-		if(soma1 >= 50) 
+		if(soma1 >= 50) //Se o jogador 1 acumular 50 pontos ou mais ele ganha
 		{
 			printf("\nParabens! %s ganhou o jogo.  \t\t\t\t(guarde esse numero: 6)", nome1);
 			
@@ -265,14 +265,14 @@ int main(){
 			scanf("%d", &repetir);
 			if(repetir == 1)
 			{
-				system("cls"); 
-				goto jogo3; 
+				system("cls"); //Limpa a tela
+				goto jogo3; //Reinicia o jogo desde o inicio
 			}
 	
 			else
 			{
-				system("cls"); 
-				goto menuEasteregg; 
+				system("cls"); //Limpa a tela
+				goto menuEasteregg; //Abre o menu com easter egg
 			}
 		}
 		
@@ -281,17 +281,17 @@ int main(){
 			printf("\n\nPressione 1 para finalizar o turno ou qualquer outro numero para continuar ");
 			scanf("%d", &continuarFinalizar);
 	
-			if(continuarFinalizar == 1) 
+			if(continuarFinalizar == 1) //Se o jogador digitar 1, comeca o turno do outro jogador
 			{
-				system ("cls");
+				system ("cls"); //Limpar a tela
 				printf("Turno do jogador %s", nome2);
-				goto jogo32; 
+				goto jogo32; //Comeca o turno do jogador 2
 			}
 
-			else 
+			else //Se o jogador digitar outro numero, ele continua seu turno
 			{
-				system ("cls"); 
-				goto jogo31; 
+				system ("cls"); //Limpar a tela
+				goto jogo31; //Jogador 1 continua jogando
 			}
 		}
 		
@@ -301,16 +301,16 @@ int main(){
 	
 		if(jogarDados>=0)
 		{
-			j2dado1 = rand() % 6; 
-		 	j2dado2 = rand() % 6; 
+			j2dado1 = rand() % 6; //Gera um numero entre 0 e 5 aleatoriamente
+		 	j2dado2 = rand() % 6; //Gera um numero entre 0 e 5 aleatoriamente
 		}
 	
-		j2dado1++; 
-		j2dado2++; 
+		j2dado1++; //Soma 1 ao numero gerado
+		j2dado2++; //Soma 1 ao numero gerado
 		
 		printf("\nDado 1: %d \t Dado 2: %d", j2dado1, j2dado2);
 		
-		if(j2dado1 == 1 && j2dado2 == 1) 
+		if(j2dado1 == 1 && j2dado2 == 1) //Se cair 1 nos dois dados o jogador perde todos os pontos
 		{
 			j2dado1 = 0;
 			j2dado2 = 0;
@@ -325,12 +325,12 @@ int main(){
 			scanf("%d", &repetir);
 			if(repetir >= 0)
 			{
-				system("cls"); 
-				goto jogo31; 
+				system("cls"); //Limpar a tela
+				goto jogo31; //Comeca o turno do jogador 1
 			}
 		}
 		
-		else if(j2dado1 == 1 || j2dado2 == 1) 
+		else if(j2dado1 == 1 || j2dado2 == 1) //Se cair 1 em um dos dados o jogador perde os pontos do turno
 		{
 			j2dado1 = 0;
 			j2dado2 = 0;
@@ -344,8 +344,8 @@ int main(){
 			scanf("%d", &repetir);
 			if(repetir >= 0)
 			{
-				system("cls"); 
-				goto jogo31; 
+				system("cls"); //Limpar a tela
+				goto jogo31; //Comeca o turno do jogador 1
 			}
 		}
 		else
@@ -355,7 +355,7 @@ int main(){
 			printf("\n\nSoma total: %d\nSoma do turno: %d", soma2, somaturno2);
 		}
 		
-		if(soma2 >= 50) 
+		if(soma2 >= 50) //Se o jogador 2 acumular 50 pontos ou mais ele ganha
 		{
 			printf("\nParabens! %s ganhou o jogo.  \t\t\t\t(guarde esse numero: 6)", nome2);
 			
@@ -363,14 +363,14 @@ int main(){
 			scanf("%d", &repetir);
 			if(repetir == 1)
 			{
-				system("cls"); 
-				goto jogo3;
+				system("cls"); //Limpa a tela
+				goto jogo3; //Reinicia o jogo desde o inicio
 			}
 	
 			else
 			{
-				system("cls"); 
-				goto menuEasteregg;
+				system("cls"); //Limpa a tela
+				goto menuEasteregg; //Abre o menu com easter egg
 			}
 		}
 		
@@ -379,17 +379,17 @@ int main(){
 			printf("\n\nPressione 1 para finalizar o turno ou qualquer outro numero para continuar ");
 			scanf("%d", &continuarFinalizar);
 	
-			if(continuarFinalizar == 1) 
+			if(continuarFinalizar == 1) //Se o jogador digitar 1, comeca o turno do outro jogador
 			{
-				system ("cls"); 
+				system ("cls"); //Limpa a tela
 				printf("Turno do jogador %s", nome1);
-				goto jogo31; 
+				goto jogo31; //Comeca o turno do jogador 1
 			}
 
 			else
 			{
-				system ("cls"); 
-				goto jogo32; 
+				system ("cls"); //Limpa a tela
+				goto jogo32; //Jogador 2 continua jogando
 			}
 		}
 	}
@@ -398,27 +398,27 @@ int main(){
 	printf("*****Menu dos jogos*****\n\n[1]Pergunta e Resposta\n[2]Adivinhe o numero\n[3]Olho de cobra\n[4]Sair\n[5]Easter egg\n");
 	scanf("%d", &comando);
 	
-	if(comando == 1) 
+	if(comando == 1) //Jogar jogo 1
 	{
 		goto jogo1;
 	}
-	if(comando == 2) 
+	if(comando == 2) //Jogar jogo 2
 	{
 		goto jogo2;
 	}
-	if(comando == 3) 
+	if(comando == 3) //Jogar jogo 3
 	{
 		goto jogo3;
 	}
-	if(comando == 4) 
+	if(comando == 4) //Sair
 	{
 		goto sair;
 	}
 	
-	if(comando == 5) 
+	if(comando == 5) //Jogar easter egg
 	{
 		jogo416:
-		system("cls");
+		system("cls"); //Limpar a tela
 		printf("Digite a senha: ");
 		scanf("%d", &senha);
 		
@@ -438,7 +438,7 @@ int main(){
 				scanf("%d", &repetir);
 				if(repetir >= 0)
 				{
-					goto menu; 
+					goto menu; //Voltar para o menu
 				}
 			}
 
@@ -461,7 +461,7 @@ int main(){
 						scanf("%d", &repetir);
 						if(repetir >= 0)
 						{
-							goto menu; 
+							goto menu; //Voltar para o menu
 						}
 					}
 					
@@ -472,7 +472,7 @@ int main(){
 						scanf("%d", &repetir);
 						if(repetir >= 0)
 						{
-							goto menu; 
+							goto menu; //Voltar para o menu
 						}
 					}
 				}
@@ -485,7 +485,7 @@ int main(){
 					scanf("%d", &repetir);
 						if(repetir >= 0)
 						{
-							goto jogo416; 
+							goto jogo416; //Digitar a senha novamente
 						}
 			}
 	}
